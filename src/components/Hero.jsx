8a2ca8bb47eba38bb30117d93f300c5e598b66e7
shivanshu <shivanshumangal007@gmail.com>
2026-02-01 13:00:54 +0530

    added background video animation with scroll and mobiel effect

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,27 +1,31 @@
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
 import { SplitText } from "gsap/all";
-import React from "react";
+import React, { useRef } from "react";
+import { useMediaQuery } from "react-responsive";
 
 const Hero = () => {
-
+  const videoRef = useRef();
+  let isMobile = useMediaQuery({maxWidth : 767})
   useGSAP(() => {
-    const loaderTimeline = gsap.timeline()
-    const headingText = new SplitText("#hero > h1", {type: "chars, words"})
-    const paragraphText = new SplitText(".body p", {type: "lines"})
+    const loaderTimeline = gsap.timeline();
+    const headingText = new SplitText("#hero > h1", { type: "chars, words" });
+    const paragraphText = new SplitText(".body p", { type: "lines" });
     headingText.chars.forEach((char) => char.classList.add("text-gradient"));
-    loaderTimeline.from(headingText.chars,{
-      yPercent: 80,
-      duration: 0.6,
-      stagger: 0.02,
-    }).from(paragraphText.lines,{
-      opacity: 0,
-      yPercent: 80,
-      stagger: 0.06,
-      duration: 0.8,
-    }, );
-  },[])
-  useGSAP(()=>{
+    loaderTimeline
+      .from(headingText.chars, {
+        yPercent: 80,
+        duration: 0.6,
+        stagger: 0.02,
+      })
+      .from(paragraphText.lines, {
+        opacity: 0,
+        yPercent: 80,
+        stagger: 0.06,
+        duration: 0.8,
+      });
+  }, []);
+  useGSAP(() => {
     gsap.to(".left-leaf", {
       yPercent: -40,
       scrollTrigger: {
@@ -31,17 +35,40 @@ const Hero = () => {
         scrub: true,
       },
     });
-    gsap.to(".right-leaf", {
-      yPercent: 40,
+    gsap.to(
+      ".right-leaf",
+      {
+        yPercent: 40,
+        scrollTrigger: {
+          trigger: "#hero",
+          start: "bottom bottom",
+          end: "bottom top",
+          scrub: true,
+        },
+      },
+      [],
+    );
+    const startValue = isMobile ? "top 40%" : "top 20%";
+    const endValue = isMobile ? "150% top" : "bottom top";
+    const video = videoRef.current;
+    const videoTl = gsap.timeline({
       scrollTrigger: {
-        trigger: "#hero",
-        start: "bottom bottom",
-        end: "bottom top",
+        trigger: video,
+        start: startValue,
+        end: endValue,
+        // start: "top 20%",
+        // end: "bottom top",
         scrub: true,
+        pin: true,
       },
     });
-
-  })
+    video.addEventListener("loadedmetadata", () => {
+      videoTl.to(video, {
+        currentTime: video.duration,
+        ease: "none",
+      });
+    });
+  });
   return (
     <>
       <section id="hero" className="noisy">
@@ -73,7 +100,15 @@ const Hero = () => {
           </div>
         </div>
       </section>
-      <div className="w-full h-screen bg-amber-950"></div>
+      <div className="video absolute inset-0">
+        <video
+          ref={videoRef}
+          src="/videos/output.mp4"
+          muted
+          playsInline
+          preload="auto"
+        />
+      </div>
     </>
   );
 };
